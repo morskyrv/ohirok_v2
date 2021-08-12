5d8e6e2655706a703b579c5b3c94d6e0265e5517
GitHub Action <action@github.com>
2021-08-12 18:44:05 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "ohirok_v2"
--- src/main/kotlin/com/github/morskyrv/ohirokv2/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/morskyrv/ohirokv2/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.morskyrv.ohirokv2.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.morskyrv.ohirokv2.MyBundle
 
 class MyApplicationService {
 
